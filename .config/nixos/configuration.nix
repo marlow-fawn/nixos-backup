bda22a5f2716f283e45454e51c9b792d1320eb15
marlow-fawn <marlow.fawn@gmail.com>
2025-03-18 16:22:41 -0400

    Generally all set up! Next step is looks, I think

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -11,14 +11,17 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
-  # Set vim as default sudo editor
-  security.sudo.extraConfig = ''
-    Defaults editor=/run/current-system/sw/bin/vim
-  '';
+  # GPU
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "intel" ];
+  
 
   # Networking
   networking.hostName = "marlow-nixos"; 
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+  
+  # Bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   services.blueman.enable = true;
 
@@ -27,37 +30,42 @@
   
   # Env vars
   environment.variables = {
-    GTK_THEME = "Adwaita:dark";
     EDITOR = "vim";
+    XDG_CURRENT_DESKTOP = "sway";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_PORTAL_FORCE = "1";
   };
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
+  
+  # Users
   users.users.marlow = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
   };
 
+  ### Packages
+
   # Packages
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    google-chrome
+    discord-ptb
+    tree
+    brightnessctl
+    xdg-desktop-portal xdg-desktop-portal-wlr
+  ];
   nixpkgs.config.allowUnfree = true; 
   
-  programs.light = {
-    enable = true;
-    brightnessKeys.enable = true;
-  };
+  # Font Packages
+  fonts.packages = with pkgs; [
+    fira-code fira-code-symbols
+    font-awesome
+    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji
+  ];
   
+  ### Programs
+
+  # Sway
   programs.sway = {
     enable = true;
     extraPackages = with pkgs; [
@@ -67,84 +75,52 @@
       wlr-randr
       mako
       swaylock
-      waybar
+      yambar
+      fuzzel
     ]; 
   };
-
-
+  
+  # Vim
   programs.vim = {
     enable = true;  
   };
   
+  # Set vim as default sudo editor
+  security.sudo.extraConfig = ''
+    Defaults editor=/run/current-system/sw/bin/vim
+  '';
+  
+  ### Services
+  
+  # Display manager, login screen
   services.displayManager = {
     ly.enable = true;
-    #autoLogin.enable = true;
-    #autoLogin.user = "marlow";
     defaultSession = "sway";
   };
 
-  environment.systemPackages = with pkgs; [
-    git
-    wget
-    google-chrome
-    discord-ptb
-    tree
-    brightnessctl
-    greetd.wlgreet
-    greetd.gtkgreet
-  ];
+  # Power settings
+  services.tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-  fonts.packages = with pkgs; [
-    fira-code
-    fira-code-symbols
-  ];
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+         #Optional helps save long term battery health
+         START_CHARGE_THRESH_BAT0 = 50; 
+         STOP_CHARGE_THRESH_BAT0 = 90;
+
+        };
+  };
+
+  # Printing
+  services.printing.enable = true;
   
-  # GPU Hardware
-  hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [ "intel" ];
+  #Audio
+  services.pipewire.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  system.stateVersion = "24.11";
 }
 
